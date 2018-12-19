06f5abe782710f7e29881f14acf244129eb1e37e
hellododa <halododa@gmail.com>
2018-12-19 23:06:53 +0900

    [#1][ADD] TEST 코드 추가

---
+++ Stack/test/stack/GenericStackTest.java
@@ -0,0 +1,34 @@
+package stack;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Project : Doda-DataStructure
+ * Time : 19/12/2018 11:05 PM
+ * Created By @hellododa
+ */
+
+public class GenericStackTest {
+
+    @Test
+    public void push() {
+    }
+
+    @Test
+    public void pop() {
+    }
+
+    @Test
+    public void isEmpty() {
+    }
+
+    @Test
+    public void peek() {
+    }
+
+    @Test
+    public void search() {
+    }
+}
